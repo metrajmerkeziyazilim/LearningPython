910fe1bde617dfe148799abd347026e1067e14e2
metrajmerkeziyazilim <metrajmerkezi@gmail.com>
2023-10-31 22:43:15 +0300

    third commit

---
+++ Ders.4.1-AssigmentOperators.py
@@ -0,0 +1,10 @@
+x,y,z=5,10,20
+print(x,y,z)
+
+
+x=x+5
+print(x)
+
+
+x+=5
+print(x)
---
+++ Ders.4.2-Example.py
@@ -0,0 +1,20 @@
+x, y, z = 2, 5, 10
+
+
+""" a = int(input("Birinci Sayıyı Giriniz...:"))
+b = int(input("İkinci Sayıyı Giriniz...:"))
+
+
+result = (a * b) - (x + y + z)
+"""
+
+result = y // x
+
+toplam = x + y + z
+result = toplam % 3
+
+result = y**x
+numbers = 1, 5, 7, 10, 6
+x, *y, z = numbers
+result = z**3
+print(result)
