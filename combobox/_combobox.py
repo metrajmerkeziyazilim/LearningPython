d84a1878fb7506cacb7213cc4331e70d0d295b5d
metrajmerkeziyazilim <metrajmerkezi@gmail.com>
2023-11-10 21:37:27 +0300

    combobox usage added in the repo today

---
+++ combobox/_combobox.py
@@ -0,0 +1,53 @@
+from PyQt6 import QtWidgets
+import sys
+from _comboboxForm import Ui_MainWindow
+
+
+class Window(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        combo = self.ui.cbSehirler
+        """combo.addItem("Ankara")
+        combo.addItem("İstanbul")
+        combo.addItem("Kocaeli")
+        combo.addItem("İzmir")
+        combo.addItems(["Mersin", "Van", "Diyarbakır"]) """
+
+        self.ui.btnLoadItem.clicked.connect(self.LoadItems)
+        self.ui.btnGetItem.clicked.connect(self.GetItem)
+        self.ui.btnClear.clicked.connect(self.ClearItems)
+
+        self.ui.cbSehirler.currentIndexChanged.connect(self.SelectedChangedIndex)
+        self.ui.cbSehirler.currentTextChanged.connect(self.SelectedChangedText)
+
+    def ClearItems(self):
+        self.ui.cbSehirler.clear()
+
+    def LoadItems(self):
+        sehirler = ["Kırıkale", "Çanakkale", "Ardahan"]
+        self.ui.cbSehirler.addItems(sehirler)
+
+    def GetItem(self):
+        print(self.ui.cbSehirler.currentText())
+        print(self.ui.cbSehirler.currentIndex())
+
+        count = self.ui.cbSehirler.count()
+        for index in range(count):
+            print(self.ui.cbSehirler.itemText(index))
+
+    def SelectedChangedIndex(self, index):
+        print(index)
+
+    def SelectedChangedText(self, text):
+        print(text)
+
+
+app = QtWidgets.QApplication(sys.argv)
+win = Window()
+win.show()
+
+sys.exit(app.exec())
